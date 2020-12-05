488b2f8278fbcc54de9f7cae3ede019f874f56ed
Supatat Singkomfu <Supatat Singkomfu>
2020-12-06 02:46:02 +0700

    Edit Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
+import Footer from '../components/Footer'
 
 const Box = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 90vh;
+    height: 60vh;
 `
 
 const Home = () => {
     return (
+        <>
         <Box>
             <h1>Home</h1>
         </Box>
+        </>
     )
 }
 
