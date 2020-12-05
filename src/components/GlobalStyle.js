cd626b9aa942d6c66ae0fd15514ec3918d694353
Supatat Singkomfu <Supatat Singkomfu>
2020-12-06 02:39:29 +0700

    Add GlobalStyle.js (Reset)

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components'
+import { reset } from 'styled-reset'
+
+const GlobalStyle = createGlobalStyle`
+ ${reset}
+
+ html, body, #root {
+   height: 100vh;
+   width: 100vw;
+   /* overflow-x: hidden; */
+
+   font-family: 'Roboto', sans-serif;
+   /* font-size: 20px; */
+
+   scroll-behavior: smooth;
+ }
+
+ a {
+  text-decoration: none;
+  color: inherit;
+   &:visited{
+     color: inherit;
+   }
+ }
+ `
+export default GlobalStyle
