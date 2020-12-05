9da2ecaf21080ecae25a68a1be843d371a588957
Supatat Singkomfu <Supatat Singkomfu>
2020-12-06 02:39:43 +0700

    Edit Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import styled from 'styled-components'
 
-function Footer() {
+const MainFooter = styled.footer`
+  padding-top: 1.5rem;
+  position: relative;
+  bottom: 0;
+  width: 100%;
+  height: 150px;
+  display: flex;
+  flex-direction: column;
+  align-items:center;
+  background-color: whitesmoke;
+  h1 {
+    font-size: 30px;
+    margin-bottom: 20px;
+    padding: 5px;
+    background-color: gray;
+  }
+`
+
+const Footer = () => {
     return (
-    <>
-        
-    </>
+    <MainFooter>
+        <h1>LOGO</h1>
+        <p>2020 Artisan Digital Asia</p>
+    </MainFooter>
     )
   }
   
