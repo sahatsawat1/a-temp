f64c4c321ed0a7f0544bbecadd26ade292fb404a
Supatat Singkomfu <Supatat Singkomfu>
2020-12-07 13:58:55 +0700

    Add Graph in components

---
+++ src/components/Graph/GraphElements.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const GraphComponents = styled.div`
+    height: 90vh;
+    padding: 0 100px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: #303746;
+    margin-bottom: 50px;
+    h1 {
+        font-size: 30px;
+        font-weight: bold;
+        margin: 50px;
+    }
+`
+
+export const GraphDetails = styled.div`
+    background-color: #fff;
+    height: 90vh;
+    width: 100%;
+    border-radius: 50px;
+`
---
+++ src/components/Graph/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { GraphComponents, GraphDetails } from './GraphElements'
+
+
+const Graph = () => {
+    return (
+        <GraphComponents>
+            <h1>GRAPH</h1>
+            <GraphDetails>
+
+            </GraphDetails>
+        </GraphComponents>
+    )
+}
+
+export default Graph
