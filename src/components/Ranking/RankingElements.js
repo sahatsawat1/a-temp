76faa097efb604a5f515b12b8b9126ae730ed520
Supatat Singkomfu <Supatat Singkomfu>
2020-12-07 13:57:22 +0700

    Add Ranking in components

---
+++ src/components/Ranking/RankingElements.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const RankingComponents = styled.div`
+    height: 90vh;
+    padding: 0 100px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: #303746;
+    h1 {
+        font-size: 30px;
+        font-weight: bold;
+        margin: 50px;
+    }
+`
+
+export const RankingDetails = styled.div`
+    background-color: #fff;
+    height: 90vh;
+    width: 100%;
+    border-radius: 50px;
+`
