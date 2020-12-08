be4809c223e8fb17f5bea38100e6e1814cc4bedf
Nat <nat.wrw@gmail.com>
2020-12-08 17:16:42 +0700

    Update navbar

--- react-app/src/components/Navbar/index.js
+++ react-app/src/components/Navbar/index.js
@@ -2,33 +2,33 @@ import React from 'react'
 import { Nav, NavLogo, NavLink, Bars, NavMenu, NavBtn, NavBtnLink } from './NavBarElements'
 
 const Navbar = () => {
-    return (
-        <>
-            <Nav>
-                <NavLogo to="/">
-                    <h1>LOGO</h1>
-                </NavLogo>
-                <Bars />
-                <NavMenu>
-                    <NavLink to="/MAP" activeStyle>
-                        MAP
-                        </NavLink>
-                    <NavLink to="/REALTIME" activeStyle>
-                        REAL TIME
-                        </NavLink>
-                    <NavLink to="/GRAPH" activeStyle>
-                        GRAPH
-                        </NavLink>
-                    <NavLink to="/RANKING" activeStyle>
-                        RANKING
-                        </NavLink>
-                </NavMenu>
-                {/* <NavBtn>
+  return (
+    <>
+      <Nav>
+        <NavLogo to="/">
+          <h1>-LOGO-</h1>
+        </NavLogo>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/MAP" activeStyle>
+            MAP
+          </NavLink>
+          <NavLink to="/REALTIME" activeStyle>
+            REAL TIME
+          </NavLink>
+          <NavLink to="/GRAPH" activeStyle>
+            GRAPH
+          </NavLink>
+          <NavLink to="/RANKING" activeStyle>
+            RANKING
+          </NavLink>
+        </NavMenu>
+        {/* <NavBtn>
                         <NavBtnLink to="/signin">Sign In</NavBtnLink>
                     </NavBtn> */}
-            </Nav>
-        </>
-    )
+      </Nav>
+    </>
+  )
 }
 
 export default Navbar
